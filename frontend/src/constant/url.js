1e851d3c0fea9fff7b570b4e2a723323fcc5c0e2
MONSTER-SARAVANAN <saravananstr600@gmail.com>
2025-07-06 02:56:29 +0530

    Katham

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,4 @@
 // ✅ Correct backend URL
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+
+export const baseUrl = "https://x-clone-k9fb.onrender.com";
